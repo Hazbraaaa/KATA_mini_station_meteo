5e04368af3f143c147c5cccb0b52011e2c8911bb
chazera <hazera.c@gmail.com>
2025-03-28 11:01:56 +0100

    etape 3, mais etape 4 aussi

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const searchButton = document.querySelector("#search-button");
 const cityInput = document.querySelector("#cityInput");
 const city = document.querySelector("#city");
 const gps = document.querySelector("#gps");
+const temperature = document.querySelector("#temperature");
+const details = document.querySelector("#details");
 
 
 // init functions
@@ -13,12 +15,36 @@ async function fetchCoordinates(cityName) {
         const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${cityName}&format=json&addressdetails=1&limit=1`);
         const data = await response.json();
 
-        city.innerText = data[0].address.city;
+        if (data[0].addresstype === "city") {
+            city.innerText = data[0].address.city;
+        }
+        else if (data[0].addresstype === "town") {
+            city.innerText = data[0].address.town;
+        }
+        else if (data[0].addresstype === "village") {
+            city.innerText = data[0].address.village;
+        }
         gps.innerText = `Coordonnées GPS: ${data[0].lat}, ${data[0].lon}`;
+        fetchWeather(data[0].lat, data[0].lon);
     } catch (error) {
         console.error(error);
         city.innerText = "Ville non trouvée";
         gps.innerText = "-";
+        temperature.innerText = "-°C";
+        details.innerText = "Vérifier le nom de la ville";
+    }
+}
+
+async function fetchWeather(lat, lon) {
+    try {
+        const response = await fetch(`http://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,precipitation,relative_humidity_2m`);
+        const data = await response.json();
+        
+        temperature.innerText = `${data.current.temperature_2m}°C`;
+        details.innerText = "Température actuelle";
+    } catch (error) {
+        console.error(error);
+        temperature.innerText = "-°C";
     }
 }
 
