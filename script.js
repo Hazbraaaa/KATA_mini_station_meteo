ddd3ecffde435e1d35a55f43a9503c5572dcfb16
chazera <hazera.c@gmail.com>
2025-03-28 12:30:13 +0100

    end of Kata. ajout des chart.js

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ const temperature = document.querySelector("#temperature");
 const details = document.querySelector("#details");
 const humidity = document.querySelector("#humidity");
 const precipitation = document.querySelector("#precipitation");
-
+const lastThreeDays = document.querySelector("#last-three-days");
+const canvasContainer = document.querySelector(".canvas-container")
+let tempChart = new Chart(lastThreeDays, {
+    type: 'bar',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '°C',
+            data: [],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 // init functions
 
@@ -28,6 +47,7 @@ async function fetchCoordinates(cityName) {
         }
         gps.innerText = `Coordonnées GPS: ${data[0].lat}, ${data[0].lon}`;
         fetchWeather(data[0].lat, data[0].lon);
+        fetchLastThreeTemp(data[0].lat, data[0].lon);
     } catch (error) {
         console.error(error);
         city.innerText = "Ville non trouvée";
@@ -43,7 +63,7 @@ async function fetchWeather(lat, lon) {
     try {
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,precipitation,relative_humidity_2m`);
         const data = await response.json();
-        
+
         temperature.innerText = `${data.current.temperature_2m}°C`;
         humidity.innerText = `${data.current.relative_humidity_2m}%`;
         precipitation.innerText = `${data.current.precipitation}mm`;
@@ -56,6 +76,35 @@ async function fetchWeather(lat, lon) {
     }
 }
 
+async function fetchLastThreeTemp(lat, lon) {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,precipitation&past_days=2`);
+        const data = await response.json();
+
+        canvasContainer.style = "visibility: visible;";
+        tempChart.destroy();
+        tempChart = new Chart(lastThreeDays, {
+            type: 'bar',
+            data: {
+                labels: data.hourly.time,
+                datasets: [{
+                    label: '°C',
+                    data: data.hourly.temperature_2m,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 // execute code
 
