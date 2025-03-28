deac10e54f1246b71db0bc97a08fac8ebdf3baf4
chazera <hazera.c@gmail.com>
2025-03-28 11:26:09 +0100

    correction CORS Api Open Meteo, pour github pages

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ async function fetchCoordinates(cityName) {
 
 async function fetchWeather(lat, lon) {
     try {
-        const response = await fetch(`http://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,precipitation,relative_humidity_2m`);
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,precipitation,relative_humidity_2m`);
         const data = await response.json();
         
         temperature.innerText = `${data.current.temperature_2m}°C`;
